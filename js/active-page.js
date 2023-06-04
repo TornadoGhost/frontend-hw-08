67b8f9494670938e08697a392192dfd12354a6d4
TornadoGhost <tornadoghost97@gmail.com>
2023-06-04 21:10:03 +0300

    start

---
+++ js/active-page.js
@@ -0,0 +1,16 @@
+//Get current URL
+let currentUrl = window.location.href;
+
+//Get the anchor elements of the tabs
+let homeTab = document.getElementById("home-tab");
+let portfolioTab = document.getElementById("portfolio-tab");
+let contanactsTab = document.getElementById("contacts-tab");
+
+//Check if the current URL matches the URL of each tab, and add the "active" class to the corresponding tab
+if (currentUrl.includes("index.html")) {
+    homeTab.classList.add("active-tab");
+} else if (currentUrl.includes("portfolio.html")) {
+    portfolioTab.classList.add("active-tab");
+} else if (currentUrl.includes("contacts.html")) {
+    contanactsTab.classList.add("active-tab");
+}
